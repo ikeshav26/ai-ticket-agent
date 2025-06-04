a72ed75494855dbf79e75d0da11d485f73f1104c
MannuVilasara <mannuvilasara@gmail.com>
2025-06-04 21:54:15 +0530

    Fix Theme not consistent bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionProviderWrapper from "@/components/sessionProvider";
+import { ThemeProvider } from "@/components/themeProvider";
 import Navbar from "@/components/myComponents/Navbar";
 import { Toaster } from "react-hot-toast";
 
@@ -31,9 +32,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProviderWrapper>
-          <Navbar />
-           <Toaster position="top-right" />
-          {children}
+          <ThemeProvider>
+            <Navbar />
+            <Toaster position="top-right" />
+            {children}
+          </ThemeProvider>
         </SessionProviderWrapper>
       </body>
     </html>
--- src/components/myComponents/Signup.tsx
+++ src/components/myComponents/Signup.tsx
@@ -6,8 +6,7 @@ import { signIn } from "next-auth/react";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
-import  toast from "react-hot-toast"
-
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
   const [form, setForm] = useState({ email: "", password: "" });
@@ -42,47 +41,82 @@ export default function SignupPage() {
       }
     } catch (err: any) {
       console.error(err);
-      toast.error(err.response?.data?.message || "Signup failed. Please try again.");
+      toast.error(
+        err.response?.data?.message || "Signup failed. Please try again."
+      );
     } finally {
       setLoading(false);
     }
   };
-
-
 
   return (
     <div className={`signupMain h-screen w-full pt-20 `}>
       <div className="flex flex-col gap-6 justify-center items-center">
         <div className="flex flex-col gap-2 items-center justify-center">
           <div className="bg-cover h-[36vh] w-[80vw] lg:w-[60vw] rounded-xl overflow-hidden flex items-center justify-center">
-            <img src="/ticket.png" alt="Ticket" className="w-full h-full no-repeat bg-cover" />
+            <div className="relative h-[36vh] w-[80vw] lg:w-[60vw] rounded-xl overflow-hidden">
+              <Image
+                src="/ticket1.png"
+                alt="Ticket"
+                fill
+                className="object-cover"
+              />
+            </div>
           </div>
-          <p className="text-xl mt-2 font-semibold uppercase">Signup for AI Ticket</p>
+          <p className="text-xl mt-2 font-semibold uppercase">
+            Signup for AI Ticket
+          </p>
         </div>
         <div className=" flex flex-col gap-2 md:gap-4 lg:gap-6 items-center justify-center">
           <form onSubmit={handleSignup}>
-          <div>
-            <p>Username</p>
-            <fieldset className="fieldset">
-              <input type="email" name="email" value={form.email} onChange={handleChange} required className="input w-60 md:w-80 lg:w-100 rounded-full focus:outline-none" placeholder="Enter Your Email" />
-            </fieldset>
-          </div>
+            <div>
+              <p>Username</p>
+              <fieldset className="fieldset">
+                <input
+                  type="email"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  required
+                  className="input w-60 md:w-80 lg:w-100 rounded-full focus:outline-none"
+                  placeholder="Enter Your Email"
+                />
+              </fieldset>
+            </div>
 
-          <div>
-            <p>Password</p>
-            <fieldset className="fieldset">
-              <input type="password" name="password" value={form.password} onChange={handleChange} required className="input w-60 md:w-80 lg:w-100 rounded-full focus:outline-none" placeholder="Enter Your Password" />
-            </fieldset>
-          </div>
+            <div>
+              <p>Password</p>
+              <fieldset className="fieldset">
+                <input
+                  type="password"
+                  name="password"
+                  value={form.password}
+                  onChange={handleChange}
+                  required
+                  className="input w-60 md:w-80 lg:w-100 rounded-full focus:outline-none"
+                  placeholder="Enter Your Password"
+                />
+              </fieldset>
+            </div>
 
-          <div>
-            <button type="submit" disabled={loading} className="bttn w-60 md:w-80 lg:w-100 rounded-full py-2 px-1 mt-3 md:mt-2 cursor-pointer">{loading?"Signing up..":"Sign Up"}</button>
-          </div>
-
+            <div>
+              <button
+                type="submit"
+                disabled={loading}
+                className="bttn w-60 md:w-80 lg:w-100 rounded-full py-2 px-1 mt-3 md:mt-2 cursor-pointer"
+              >
+                {loading ? "Signing up.." : "Sign Up"}
+              </button>
+            </div>
           </form>
         </div>
         <div>
-          <p className="text-sm text-gray-500">Already have an account? <Link href="/signin" className="text-blue-500 hover:underline">Signin</Link></p>
+          <p className="text-sm text-gray-500">
+            Already have an account?{" "}
+            <Link href="/signin" className="text-blue-500 hover:underline">
+              Signin
+            </Link>
+          </p>
         </div>
       </div>
     </div>
